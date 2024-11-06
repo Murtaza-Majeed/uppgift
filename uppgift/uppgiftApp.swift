8b93555e9d8b0e05b025194e883fa52195408025
Murtaza Majeed <murtaza.majeed@hotmail.com>
2024-11-06 15:22:57 +0100

    Initial Commit

---
+++ uppgift/uppgiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  uppgiftApp.swift
+//  uppgift
+//
+//  Created by Murtaza Majeed on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct uppgiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ uppgiftTests/uppgiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  uppgiftTests.swift
+//  uppgiftTests
+//
+//  Created by Murtaza Majeed on 2024-11-06.
+//
+
+import Testing
+@testable import uppgift
+
+struct uppgiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
